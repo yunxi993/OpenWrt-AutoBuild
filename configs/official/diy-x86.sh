ee3557beee8f09f47e255812c79c17fd65b19a3b
Sil <59167936+yunxi993@users.noreply.github.com>
2025-02-23 04:59:33 +0800

    Number of compilations from 4 to 2.

--- configs/official/diy-x86.sh
+++ configs/official/diy-x86.sh
@@ -28,8 +28,8 @@ sed -i 's/OpenWrt/nzksm/g' package/base-files/files/bin/config_generate
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # Add additional packages
-#rm -rf feeds/packages/net/xray-core
-#rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/{xray-core,v2ray-core,sing-box}
+rm -rf feeds/luci/applications/{luci-app-ssr-plus, luci-app-passwall, luci-app-passwall2}
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
 git clone --depth=1 https://github.com/yunxi993/extra.git package/extra
